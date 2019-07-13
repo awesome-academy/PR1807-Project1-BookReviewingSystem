affa79160509a4316ba13abe477c1876315da4b2
hao truong <haotv03@gmail.com>
2019-07-13 11:41:19 +0700

    Custom Errors Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    match "/404", to: "errors#file_not_found", via: :all
+    match "/422", to: "errors#unprocessable", via: :all
+    match "/500", to: "errors#internal_server_error", via: :all
     mount Ckeditor::Engine => '/ckeditor'
-    root 'cates#index'
-    get '/signup', to: 'users#new'
-    post '/signup',  to: 'users#create'
-    get    '/login',   to: 'sessions#new'
-    post   '/login',   to: 'sessions#create'
-    delete '/logout',  to: 'sessions#destroy'
+    root "cates#index"
+    get "/signup", to: "users#new"
+    post "/signup",  to: "users#create"
+    get    "/login",   to: "sessions#new"
+    post   "/login",   to: "sessions#create"
+    delete "/logout",  to: "sessions#destroy"
     resources :users
     resources :cates
     resources :books
     resources :chapters
 
     namespace :admin do
-      root 'book_pages#home'
-      get    '/login',   to: 'sessions#new'
-      post   '/login',   to: 'sessions#create'
-      delete '/logout',  to: 'sessions#destroy'
+      root "book_pages#home"
+      get    "/login",   to: "sessions#new"
+      post   "/login",   to: "sessions#create"
+      delete "/logout",  to: "sessions#destroy"
       resources :users
       resources :cates
       resources :books
